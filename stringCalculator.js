d306ae13b87d01e830aa2d0557b1626e71cae36a
meShantanu <shantmiitanphy@gmail.com>
2025-03-05 20:25:31 +0530

    Support Custom Delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,14 @@
 function add(numbers) {
   if (numbers === '') return 0
-  return numbers.split(/,|\n/).reduce((sum, num) => sum + parseInt(num), 0)
+
+  let delimiter = /,|\n/
+  if (numbers.startsWith('//')) {
+    let parts = numbers.split('\n')
+    delimiter = new RegExp(parts[0].slice(2))
+    numbers = parts[1]
+  }
+
+  return numbers.split(delimiter).reduce((sum, num) => sum + parseInt(num), 0)
 }
 
 module.exports = add
